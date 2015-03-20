21f780b941d38ff497788983c40f8052644f3af9
Diana <crazykilla15@gmail.com>
2015-03-20 17:58:54 -0400

    Licenses stuff

--- RbxAPI/inputPass.py
+++ RbxAPI/inputPass.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 # ROBLOX Trade Currency bot.
 # Copyright (C) 2015  Diana S. Land
-
-# This program is free software; you can redistribute it and/or modify
+# This file is a part of the ROBLOX Trade Currency bot.
+# ROBLOX Trade Currency bot. is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
+# ROBLOX Trade Currency bot. is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# with ROBLOX Trade Currency bot.; if not, If not, see <http://www.gnu.org/licenses/> and contact Diana
 
 # Diana may be contacted online at CrazyKilla15@gmail.com
 
--- RbxAPI/trade.py
+++ RbxAPI/trade.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# ROBLOX Trade Currency bot.
+# Copyright (C) 2015  Diana S. Land
+# This file is a part of the ROBLOX Trade Currency bot.
+# ROBLOX Trade Currency bot. is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# ROBLOX Trade Currency bot. is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with ROBLOX Trade Currency bot.; if not, If not, see <http://www.gnu.org/licenses/> and contact Diana
+
+# Diana may be contacted online at CrazyKilla15@gmail.com
 """
 Created on Aug 2, 2014
 
